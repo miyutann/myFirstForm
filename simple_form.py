d9501ac6459e76bc26be1e2c590073d31a0192e2
miy <ku_miyu@outlook.jp>
2022-12-14 22:59:42 +0900

    addition app.

--- simple_form.py
+++ simple_form.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 def index():
  return render_template("start.html")
 
-
-
 @app.route('/procForm', methods=['POST'])
 def processPost():
- msg = request.form['msg']
- return render_template("result.html", message = msg)
+ msg1 = request.form['msg1']
+ msg2 = request.form['msg2']
+ msg1 = int(msg1)
+ msg2 = int(msg2)
+ return render_template("result.html", message = msg1 + msg2)
 if __name__ == '__main__':
  app.run(host="0.0.0.0", port=8000, debug = True)
